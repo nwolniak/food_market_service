9a4cf5f724e28312af7f7038efaa152e67bc80b9
nwolniak <nwolniak7@gmail.com>
2023-08-17 11:50:46 +0200

    cart items ordering and paying - frontend

--- frontend/src/app/_models/_domain/cart.ts
+++ frontend/src/app/_models/_domain/cart.ts
@@ -3,10 +3,12 @@ import {ItemQuantity} from "@app/_models";
 export class Cart {
   id: string;
   cartItems: ItemQuantity[];
+  price: number;
   isAction?: boolean;
 
-  constructor(id: string, cartItems: ItemQuantity[]) {
+  constructor(id: string, cartItems: ItemQuantity[], price: number) {
     this.id = id;
+    this.price = price;
     this.cartItems = cartItems;
   }
 
